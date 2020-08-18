9e59e7d9fffc024965ecedc36d5547e576cc307a
karina-hensel <henselkarina@gmail.com>
2020-08-18 09:25:37 +0200

    Some changes

--- Model/utils.py
+++ Model/utils.py
@@ -4,6 +4,8 @@ import codecs
 
 """ Utility functions to load the data sets and preprocess the input """
 
+TAG_INDICES = {'I-PER':0, 'B-PER':1, 'I-LOC':2, 'B-LOC':3, 'I-ORG':4, 'B-ORG':5, 'I-MISC':6, 'B-MISC':7, 'O':8}
+
 def load_sentences(filepath):
     """
     Load sentences (separated by newlines) from dataset
@@ -23,9 +25,11 @@ def load_sentences(filepath):
 
     with open(filepath, 'r') as f:
         for line in f.readlines():
-           if line == ('\n'):
+           if line == ('-DOCSTART- -X- -X- O\n') or line == '\n':
+               #pass
+           #if line=='\n':
                # Sentence as a sequence of tokens, POS, chunk and NE tags
-               sentence = dict({'TOKENS' : [], 'POS' : [], 'CHUNK_TAG' : [], 'NE' : []})
+               sentence = dict({'TOKENS' : [], 'POS' : [], 'CHUNK_TAG' : [], 'NE' : [], 'SEQ' : []})
                sentence['TOKENS'] = tok
                sentence['POS'] = pos
                sentence['CHUNK_TAG'] = chunk
@@ -53,16 +57,11 @@ def load_sentences(filepath):
 
 def word_char_dicts(wor):
     """
-    Create a dictionary of all words in the file
+    Create a dictionary of all words in the file:
+        
+        sentences - list of sentence dictionaries.
 
-    Parameters
-    ----------
-    sentences : list
-        List of sentence dictionaries.
-
-    Returns
-    -------
-    Set of unique tokens
+    Returns - set of unique tokens
 
     """
     
@@ -74,21 +73,17 @@ def word_char_dicts(wor):
     # Create character dictionary
     separator = ', '
     chars = set(separator.join(words))
-    
+    words.update('PAD')
+    words.update('UNK')
     return words, chars
 
 def ne_labels_dict(sentences):
     """
     Create a dictionary of all NE labels in the file
 
-    Parameters
-    ----------
-    sentences : list
-        List of sentence dictionaries.
+    sentences - list of sentence dictionaries.
 
-    Returns
-    -------
-    Set of unique NE labels
+    Returns - Set of unique NE labels
 
     """
     
@@ -99,10 +94,22 @@ def ne_labels_dict(sentences):
     
     return labels
 
+#def build_vocab(sents, labels):
+    
+def read_conll_datasets(data_dir):
+    data = {}
+    for data_set in ["train","test","valid"]:
+        data[data_set] = load_sentences("%s/%s.txt" % (data_dir,data_set)) 
+        
+    #words = word_char_dicts(data["train"])[0]
+    return data#, words
+
 # =========================
 # Test
-sents = load_sentences('../Data/conll2003/en/test.txt')
+#sents = load_sentences('../Data/conll2003/en/test.txt')
 #print(sents)
-tokens = word_char_dicts(sents)
-labels = ne_labels_dict(sents)
-print(labels)  
+#tokens = word_char_dicts(sents)
+#labels = ne_labels_dict(sents)
+#print(labels)  
+#data = read_conll_datasets('../Data/conll2003/en')
+#print(data['train'])
