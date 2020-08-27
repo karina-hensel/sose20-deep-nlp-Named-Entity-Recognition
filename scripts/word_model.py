a49f60c57afadbc6aba0e14cecb2419abb148272
karina-hensel <38494437+karina-hensel@users.noreply.github.com>
2020-08-27 17:50:49 +0200

    Update word_model.py

--- scripts/word_model.py
+++ scripts/word_model.py
@@ -1,45 +1,52 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 
 Basic, word-based BiLSTM model
 
-Created on Tue Aug  4 18:02:19 2020
-
 @author: karina
 """
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import Module, Embedding, LSTM, Linear, NLLLoss, Dropout, CrossEntropyLoss
 import torch.optim as optim
-from torch.utils.tensorboard import SummaryWriter
-import torchtext
-from torchtext import data
 
 import gensim
 from gensim.scripts.glove2word2vec import glove2word2vec
 
 import numpy as np
-import os
-import sys
 import math
 from utils import *
 
-# Data and hyperparameters
-#embeddings_file = '../Data/embeddings/en/glove.6B.100d.bin'
-
-embeddings_file = '../data/embeddings/en/GoogleNews-pruned2tweets.txt'
-data_dir = '../data/conll2003/en/'
-model_dir = '../models/'
 
 def prepare_emb(sent, tags, words_to_ix, tags_to_ix):
+    """
+    Maps each token to its embedding
+
+    Parameters
+    ----------
+    sent : list
+        sentence to process
+    tags : list
+        tag sequence for sentence
+    words_to_ix : dict
+        maps words to embedding indices
+    tags_to_ix : dict
+        maps tag to tag indices
+
+    Returns
+    -------
+    torch.tensor
+        2 dimensional tensor containing the indices for each word and
+        the corresponding tag
+
+    """
     w_idxs, tag_idxs = [], []
     for w, t in zip(sent, tags):
         if w.lower() in words_to_ix.keys():
-            w_idxs.append(words_to_ix[w.lower()])# = [to_ix[w] for w in seq if w in to_ix.keys()]
+            w_idxs.append(words_to_ix[w.lower()])
         else:
-            # Map unknown words to the reserved embeddig vectr
+            # Map unknown words to the reserved embeddig vector
             w_idxs.append(words_to_ix['frock'])
                                       
         if t in tags_to_ix.keys():
@@ -87,64 +94,3 @@ class WordModel(Module):
         pred = self.hidden2tag(h.view(len(x), -1))
         
         return F.log_softmax(pred, dim=1)
-    
-
-'''if __name__=='__main__':
-    args = sys.argv
-    
-    EMB_FILE = str(args[0])
-    DATA_DIR = str(args[1])
-    EPOCHS = int(args[2])
-    DROPOUT = args[3]
-    MODEL_FILE = str(args[4])
-    
-    # Load data
-    data = read_conll_datasets(DATA_DIR)
-    gensim_embeds = gensim.models.KeyedVectors.load_word2vec_format(EMB_FILE, encoding='utf8')
-    pretrained_embeds = gensim_embeds.vectors 
-    
-    # To convert words in the input to indices of the embeddings matrix
-    word_to_idx = {word: i for i, word in enumerate(gensim_embeds.vocab.keys())}
-    
-    # Set hyperparameters
-    # Number of output classes (9)
-    n_classes = len(TAG_INDICES)
-    n_epochs = EPOCHS
-    p = DROPOUT
-    report_every = 1
-    
-    # Set up and initialize model
-    model = Model(pretrained_embeds, 100, len(word_to_idx), n_classes, p)
-    loss_function = NLLLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.6)
-    
-    # Training loop
-    for e in range(n_epochs+1):
-        total_loss = 0
-        for sent in data["train"][5:10]:
-            
-            # (1) Set gradient to zero for new example: Set gradients to zero before pass
-            model.zero_grad()
-            
-            # (2) Encode sentence and tag sequence as sequences of indices
-            input_sent,  gold_tags = prepare_emb(sent["TOKENS"], sent["NE"], word_to_idx, TAG_INDICES)
-            
-            # (3) Predict tags (sentence by sentence)
-            if len(input_sent) > 0:
-                pred_scores = model(input_sent)
-                
-                # (4) Compute loss and do backward step
-                loss = loss_function(pred_scores, gold_tags)
-                loss.backward()
-              
-                # (5) Optimize parameter values
-                optimizer.step()
-          
-                # (6) Accumulate loss
-                total_loss += loss
-                
-        if ((e+1) % report_every) == 0:
-            print('epoch: %d, loss: %.4f' % (e, total_loss*100/len(data['train'])))
-            
-    # Save the trained model
-    save_model(model, MODEL_FILE, optimizer, loss_function)'''
