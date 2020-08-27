f4ff0c57a48cb67d3d43dba12d3dfe6fdf46a797
karina-hensel <38494437+karina-hensel@users.noreply.github.com>
2020-08-27 17:52:00 +0200

    Update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,14 +1,18 @@
 import torch
 import torch as nn
-import codecs
 
-""" Utility functions to load the data sets and preprocess the input """
+""" Utility functions to load the data sets and preprocess the input 
+
+Created on Tue July  24 18:30:17 2020
+
+@author: karina
+"""
 
 TAG_INDICES = {'I-PER':0, 'B-PER':1, 'I-LOC':2, 'B-LOC':3, 'I-ORG':4, 'B-ORG':5, 'I-MISC':6, 'B-MISC':7, 'O':8}
 
 def load_sentences(filepath):
     """
-    Load sentences (separated by newlines) from dataset
+    Load sentences (separated by newlines) from a CONLL dataset
 
     Parameters
     ----------
@@ -26,9 +30,6 @@ def load_sentences(filepath):
     with open(filepath, 'r') as f:
         for line in f.readlines():
            if line == ('-DOCSTART- -X- -X- O\n') or line == '\n':
-               #pass
-           #if line=='\n':
-               # Sentence as a sequence of tokens, POS, chunk and NE tags
                sentence = dict({'TOKENS' : [], 'POS' : [], 'CHUNK_TAG' : [], 'NE' : [], 'SEQ' : []})
                sentence['TOKENS'] = tok
                sentence['POS'] = pos
@@ -57,7 +58,7 @@ def load_sentences(filepath):
 
 def word_char_dicts(wor):
     """
-    Create a dictionary of all words in the file:
+    Create a dictionary of all words in the file (not used anymore)
         
         sentences - list of sentence dictionaries.
 
@@ -79,7 +80,7 @@ def word_char_dicts(wor):
 
 def ne_labels_dict(sentences):
     """
-    Create a dictionary of all NE labels in the file
+    Create a dictionary of all NE labels in the file  (not used anymore)
 
     sentences - list of sentence dictionaries.
 
@@ -94,15 +95,27 @@ def ne_labels_dict(sentences):
     
     return labels
 
-#def build_vocab(sents, labels):
     
 def read_conll_datasets(data_dir):
+    """
+    Read datasets in the CONLL format
+
+    Parameters
+    ----------
+    data_dir : str
+        Directory of the data file.
+
+    Returns
+    -------
+    data : dict
+        dictionary containing all sentences from the train and test sets.
+
+    """
     data = {}
-    for data_set in ["train","test","valid"]:
+    for data_set in ["train","test"]:
         data[data_set] = load_sentences("%s/%s.txt" % (data_dir,data_set)) 
         
-    #words = word_char_dicts(data["train"])[0]
-    return data#, words
+    return data
 
 def save_model(model, name, optimizer, loss):
     """
@@ -115,10 +128,24 @@ def save_model(model, name, optimizer, loss):
     name : String
         File name.
     """
+    
     torch.save(model, name +'.pt')
     torch.save(model.state_dict(), name + '_2.pt')
     torch.save({
     'model': model.state_dict(),
     'optimizer': optimizer.state_dict(),
     'loss': loss.state_dict()
-}, name + '_state_dict.pt')
+    }, name + '_state_dict.pt')
+
+
+def load_model(model_file, device):
+    """
+    Load a model from a file and print evaluation
+
+    Parameters
+    ----------
+    model_file : String
+        Path to model file.
+    """
+    model = torch.load(model_file, map_location=device)
+    return model
