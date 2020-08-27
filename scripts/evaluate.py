ee61d53ba40d93c89b3526fb2c86ea930d47c02a
karina-hensel <38494437+karina-hensel@users.noreply.github.com>
2020-08-27 17:53:07 +0200

    Update evaluate.py

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 
-Evaluate a trained model on the test set data (work in progress)
-Created on Fri Aug 21 23:57:45 2020
+Evaluate a trained model on the test set data
 
 @author: karina
 """
@@ -13,45 +10,65 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import Module, Embedding, LSTM, Linear, NLLLoss, Dropout, CrossEntropyLoss
 import torch.optim as optim
-from torch.utils.tensorboard import SummaryWriter
-import torchtext
-from torchtext import data
 
 import gensim
 from gensim.scripts.glove2word2vec import glove2word2vec
 
 import numpy as np
-import os
-import math
-from model.utils import *
+import argparse
+from utils import *
+from word_model import WordModel, prepare_emb
+from train import *
 
-# Data and hyperparameters
-#embeddings_file = '../Data/embeddings/en/glove.6B.100d.bin'
 
-embeddings_file = '../data/embeddings/en/GoogleNews-pruned2tweets.txt'
-data_dir = '../data/conll2003/en/'
-model_dir = '../model/'
-model = 'm1.pkl'
-
-#--- test ---
-load_model(model_dir + model)
-
-correct = 0
-with torch.no_grad():
-  for sent in data["test"]:
-    input_sent,  gold_tags = prepare_emb(sent["TOKENS"], sent["NE"], word_to_idx, TAG_INDICES)
-    # WRITE CODE HERE
-    predicted, correct = 0.0, 0.0
+if __name__=='__main__':
+    # Set up to parse command line arguments
+    parser = argparse.ArgumentParser(description='Evaluate a trained model on the test set data')
     
-    # Predict class with the highest probability
-    if len(input_sent) > 0:
-        predicted = torch.argmax(model(input_sent), dim=1)
-        print(predicted)
-        print(gold_tags)
-        correct += torch.eq(predicted,gold_tags).item()
-  
-    if verbose:
-      print('TEST DATA: %s, OUTPUT: %s, GOLD TAG: %d' % 
-            (sent["TOKENS"], sent["NE"], predicted))
-      
-  print('test accuracy: %.2f' % (100.0 * correct / len(data["test"])))
+    parser.add_argument('Embedding', metavar='EMB_FILE', type=str,
+                   help='Path to word embedding file')
+    parser.add_argument('Data_file', metavar='DATA_FILE', type=str,
+                   help='Path to CONLL data set')
+    parser.add_argument('Model_file', metavar='MODEL_FILE', type=str,
+                   help='Name of the file to load the pretrained model from')
+    
+    args = parser.parse_args()
+    
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    
+    # Load data and model
+    dataset = read_conll_datasets(args.Data_file)
+    print(args.Model_file)
+    model = load_model(str(args.Model_file), device)
+    emb_file = args.Embedding
+    
+    gensim_embeds = gensim.models.KeyedVectors.load_word2vec_format(emb_file, encoding='utf8')
+    pretrained_embeds = gensim_embeds.vectors 
+    
+    # To convert words in the input to indices of the embeddings matrix
+    word_to_idx = {word: i for i, word in enumerate(gensim_embeds.vocab.keys())}
+        
+    correct, num_words, f1_scores, num_sents = 0, 0, 0, 0
+    with torch.no_grad():
+        for sent in dataset["test"]:
+            
+            num_words += len(sent["TOKENS"])
+            input_sent,  gold_tags = prepare_emb(sent["TOKENS"], sent["NE"], word_to_idx, TAG_INDICES)
+            
+            predicted, cor = 0.0, 0.0
+    
+            # Predict class with the highest probability
+            if len(input_sent) > 0:
+                num_sents += 1
+                predicted = torch.argmax(model(input_sent.to(device)), dim=1)
+                
+                correct += torch.sum(torch.eq(predicted.to(device),gold_tags.to(device)))
+                f1_scores += f1_score(predicted.cpu(), gold_tags.cpu(), average='weighted')
+                
+    if model.dropout == 0.0:
+        print('Evaluation of the word-based BiLSTM without dropout:')
+    else:
+        print('Evaluation of the word-based BiLSTM with dropout:')
+    print('----------------------------------------------------------')
+    print('Test set accuracy: %.2f' % (100.0 * correct / num_words))
+    print('Test set f1-score: %.2f' % (100.0 * f1_scores / num_sents))
